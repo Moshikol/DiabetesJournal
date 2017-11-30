d155eedf7af35e2bf1f4ba7d691aca292bed1430
moshiko lev <moshiko.lev@gmail.com>
2017-12-01 01:39:03 +0200

    release  version

--- android/app/src/main/java/com/diabetesjournal/MainApplication.java
+++ android/app/src/main/java/com/diabetesjournal/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.RNFetchBlob.RNFetchBlobPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFetchBlobPackage(),
-            new ImagePickerPackage(),
-            new VectorIconsPackage()
+            new RNFetchBlobPackage()
       );
     }
   };
