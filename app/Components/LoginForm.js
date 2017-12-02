2cfedac2aa4d057b3932b92db27ee009275833ef
moshiko lev <moshiko.lev@gmail.com>
2017-12-02 17:00:09 +0200

    new commit working partially

--- app/Components/LoginForm.js
+++ app/Components/LoginForm.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Dimensions,
+    TextInput,
+    Button,
+    TouchableOpacity
+} from 'react-native';
+//import { Button } from 'react-native-elements';
 import Toast from 'react-native-simple-toast';
 import { Actions } from 'react-native-router-flux';
 import { Card, CardSection, Input, Spinner } from './common'
 import firebase from 'firebase';
+const { width, height } = Dimensions.get("window");
+
+const background = require("../Resources/img/login1_bg.png");
+const mark = require("../Resources/img/login1_mark.png");
+const lockIcon = require("../Resources/img/login1_lock.png");
+const personIcon = require("../Resources/img/login1_person.png");
 
 class LoginForm extends Component {
     state = { email: '', pass: '', error: '', loading: false, isLoggedIn: false };
@@ -54,51 +70,141 @@ class LoginForm extends Component {
             return <Spinner size="small" />;
         }
         return (
-            <Button
-                iconRight={{ name: 'send', size: 32, color: 'white' }}
-                buttonStyle={{ backgroundColor: '#00b764', borderRadius: 10 }}
-                textStyle={{ textAlign: 'center' }}
-                fontSize={26}
-                title={`Login/Sign up`}
-                onPress={this.onLogin.bind(this)}
-            />
+            <TouchableOpacity onPress={this.onLogin.bind(this)} activeOpacity={.5}>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>Sign In</Text>
+                </View>
+            </TouchableOpacity>
         );
 
 
     }
     render() {
         return (
-            <Card>
-                <CardSection>
-                    <Input
-                        style={{ fontFamily: 'monospace' }}
-                        label='Email'
-                        keytype='email-address'
-                        placeHolder='Google@gmail.com'
-                        onChangeText={(email) => this.setState({ email })}
-                        value={this.state.email} />
-                </CardSection>
-                <CardSection>
-                    <Input
-                        securetxt
-
-                        placeHolder='password'
-                        onChangeText={(pass) => this.setState({ pass })}
-                        value={this.state.pass} />
-                </CardSection>
-                <CardSection style={{ borderRadius: 10 }}>
-                    {this.renderBtn()}
-
-                </CardSection>
-                <CardSection style={{ borderRadius: 10 }}>
-                    <Text style={{ fontSize: 20, color: 'red' }}>
-                        {this.state.error}
-                    </Text>
-                </CardSection>
-            </Card >
+            <View style={styles.container}>
+                <Image source={background} style={styles.background} resizeMode="cover">
+                    <View style={styles.markWrap}>
+                        <Image source={mark} style={styles.mark} resizeMode="contain" />
+                    </View>
+                    <View style={styles.wrapper}>
+                        <View style={styles.inputWrap}>
+                            <View style={styles.iconWrap}>
+                                <Image source={personIcon} style={styles.icon} resizeMode="contain" />
+                            </View>
+                            <TextInput
+                                placeholder="Username"
+                                placeholderTextColor="#FFF"
+                                onChangeText={(email) => this.setState({ email })}
+                                value={this.state.email}
+                                style={styles.input}
+                            />
+                        </View>
+                        <View style={styles.inputWrap}>
+                            <View style={styles.iconWrap}>
+                                <Image source={lockIcon} style={styles.icon} resizeMode="contain" />
+                            </View>
+                            <TextInput
+                                placeholderTextColor="#FFF"
+                                placeholder="Password"
+                                style={styles.input}
+                                secureTextEntry
+                                onChangeText={(pass) => this.setState({ pass })}
+                                value={this.state.pass}
+                            />
+                        </View>
+                        <TouchableOpacity activeOpacity={.5}>
+                            <View>
+                                <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+                            </View>
+                        </TouchableOpacity>
+                        {this.renderBtn()}
+                    </View>
+                    <View style={styles.container}>
+                        <View style={styles.signupWrap}>
+                            <Text style={styles.accountText}>Don't have an account?</Text>
+                            <TouchableOpacity activeOpacity={.5}>
+                                <View>
+                                    <Text style={styles.signupLinkText}>Sign Up</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Image>
+            </View>
         );
     }
 
 
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    markWrap: {
+        flex: 1,
+        paddingVertical: 30,
+    },
+    mark: {
+        width: null,
+        height: null,
+        flex: 1,
+    },
+    background: {
+        width,
+        height,
+    },
+    wrapper: {
+        paddingVertical: 30,
+    },
+    inputWrap: {
+        flexDirection: "row",
+        marginVertical: 10,
+        height: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: "#CCC"
+    },
+    iconWrap: {
+        paddingHorizontal: 7,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    icon: {
+        height: 20,
+        width: 20,
+    },
+    input: {
+        flex: 1,
+        paddingHorizontal: 10,
+    },
+    button: {
+        backgroundColor: "#FF3366",
+        paddingVertical: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 30,
+    },
+    buttonText: {
+        color: "#FFF",
+        fontSize: 18,
+    },
+    forgotPasswordText: {
+        color: "#D8D8D8",
+        backgroundColor: "transparent",
+        textAlign: "right",
+        paddingRight: 15,
+    },
+    signupWrap: {
+        backgroundColor: "transparent",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    accountText: {
+        color: "#D8D8D8"
+    },
+    signupLinkText: {
+        color: "#FFF",
+        marginLeft: 5,
+    }
+});
 export default LoginForm;
