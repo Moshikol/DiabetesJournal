afa579d4e880c7c9eef0d964be2f5fd6af1f5fc8
moshiko lev <moshiko.lev@gmail.com>
2017-12-04 13:53:39 +0200

    Added Navigation drawer

---
+++ app/Components/SideMenu.js
@@ -0,0 +1,83 @@
+import React, { Component, PropTypes } from 'react';
+import { StyleSheet, Text, View, ViewPropTypes } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import Button from 'react-native-button';
+import { CardSection } from './common/CardSection';
+
+const contextTypes = {
+    drawer: React.PropTypes.object,
+};
+
+const propTypes = {
+    name: PropTypes.string,
+    sceneStyle: ViewPropTypes.style,
+    title: PropTypes.string,
+};
+
+const styles = StyleSheet.create({
+    viewContainer: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    container: {
+        padding: 15,
+        height: 45,
+        overflow: 'hidden',
+        alignSelf: 'flex-start',
+    },
+    textStyle: {
+        fontSize: 18,
+        color: '#555',
+    },
+    nameContainer: {
+        padding: 15,
+        height: 45,
+        overflow: 'hidden',
+        alignSelf: 'flex-start',
+    },
+    name: {
+        fontSize: 22,
+        color: '#555',
+        fontWeight: '400',
+    }
+});
+
+class SideMenu extends Component {
+    render() {
+        return (
+            <View style={[styles.viewContainer, this.props.sceneStyle]}>
+                <CardSection style={{ flexDirection: 'column', padding: 30 }}>
+                    <Button
+                        containerStyle={styles.container}
+                        style={styles.name}
+                        onPress={() => { Actions.profile(); }}
+                    >User Name</Button>
+                </CardSection>
+                <CardSection style={{ flexDirection: 'column' }}>
+                    <Button
+                        containerStyle={styles.container}
+                        style={styles.textStyle}
+                        onPress={() => { Actions.home(); }}
+                    >Home Page</Button>
+                </CardSection>
+                <CardSection style={{ flexDirection: 'column', borderBottomWidth: 0, }}>
+                    <Button
+                        containerStyle={styles.container}
+                        style={styles.textStyle}
+                        onPress={() => Actions.settings()}
+                    >Settings</Button>
+                    <Button
+                        containerStyle={styles.container}
+                        style={styles.textStyle}
+                        onPress={() => Actions.auth()}
+                    >Log Out</Button>
+                </CardSection>
+            </View>
+        );
+    }
+}
+
+SideMenu.contextTypes = contextTypes;
+SideMenu.propTypes = propTypes;
+
+export default SideMenu;
--- app/Router.js
+++ app/Router.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Router, Scene, Stack,ActionConst } from 'react-native-router-flux';
+import { Router, Scene, Stack, ActionConst } from 'react-native-router-flux';
 import LoginForm from './Components/LoginForm';
 import BglstForm from './Components/bgLst';
 import NewBGFrom from './Components/newBG';
+import SideMenu from './Components/SideMenu';
 
 const RouterComponent = () => {
     return (
@@ -12,9 +13,11 @@ const RouterComponent = () => {
                     <Scene key="login" hideNavBar component={LoginForm} title="Please Login" initial />
                 </Stack>
 
-                <Stack key="BG"  hideNavBar >
-                    <Scene key="bglst"  hideNavBar={false} init component={BglstForm} title="BG Jouranl"  />
-                    <Scene key="newbg"  back={false} hideNavBar={false} component={NewBGFrom} title="Add New BG"  />
+                <Stack key="BG" hideNavBar >
+                    <Scene key="drawer" drawer contentComponent={SideMenu} >
+                        <Scene key="bglst" hideNavBar={false} init component={BglstForm} title="BG Jouranl" />
+                        <Scene key="newbg" back hideNavBar={false} component={NewBGFrom} title="Add New BG" />
+                    </Scene>
                 </Stack>
             </Stack>
 
